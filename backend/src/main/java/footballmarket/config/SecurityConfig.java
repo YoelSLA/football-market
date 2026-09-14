387b03844faf9f33da7dc92431584e65bddd9c76
YoelSLA <yoel.ventoso@gmail.com>
2026-09-14 20:38:35 -0300

    Se arreglo el build

--- backend/src/main/java/footballmarket/config/SecurityConfig.java
+++ backend/src/main/java/footballmarket/config/SecurityConfig.java
@@ -19,9 +19,15 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf.disable()) // Note: In production, consider CSRF protection if applicable
+    http.csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(
-            auth -> auth.requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated());
+            auth ->
+                auth.requestMatchers(
+                        "/api/auth/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated());
+
     return http.build();
   }
 }
